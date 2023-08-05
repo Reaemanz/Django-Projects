f150faf65e823c02381d1f5cdaa0cefe3e4dda28
Nsubuga Emmanuel Reagan <nsubugaemmanuel1@gmail.com>
2023-08-05 22:35:12 +0100

    Update urls.py

--- Hello World/Django Project/urls.py
+++ Hello World/Django Project/urls.py
@@ -1,9 +1,7 @@
 """
 URL configuration for django_project project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
+The `urlpatterns` list routes URLs to views.
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
