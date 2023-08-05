b59d52b260fe53f0a7576722cb27226f286d9760
Nsubuga Emmanuel Reagan <nsubugaemmanuel1@gmail.com>
2023-08-05 22:36:04 +0100

    Update asgi.py

--- Hello World/Django Project/asgi.py
+++ Hello World/Django Project/asgi.py
@@ -3,8 +3,7 @@ ASGI config for django_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+
 """
 
 import os
